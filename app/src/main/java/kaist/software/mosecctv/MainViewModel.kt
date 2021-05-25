e795a0d81f3e0031047464926f548b64fc879856
kkuby <kkuby8@kaist.ac.kr>
2021-05-25 17:50:05 +0900

    first

---
+++ app/src/main/java/kaist/software/mosecctv/MainViewModel.kt
@@ -0,0 +1,12 @@
+package kaist.software.mosecctv
+
+import android.arch.lifecycle.ViewModel
+import com.google.firebase.database.DatabaseReference
+
+class MainViewModel : ViewModel() {
+
+    private lateinit var database: DatabaseReference
+
+
+
+}
